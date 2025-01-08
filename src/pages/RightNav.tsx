8b20ee5ef0873245d9d81338b6656327c62c9e03
dagijosi <dagijossy18@gmail.com>
2025-01-08 16:07:10 +0300

    refactor: adjust button padding in RightNav component for consistency

--- src/pages/RightNav.tsx
+++ src/pages/RightNav.tsx
@@ -27,7 +27,7 @@ const RightNav = () => {
   return (
     <div className="bg-middleblue text-white h-screen md:block py-6 px-8 overflow-y-auto custom-scrollbar">
       <div className="flex flex-row justify-between items-center">
-        <button className=" inline-flex py-3 px-4 items-center bg-darkgray rounded-2xl justify-center">
+        <button className=" inline-flex p-4 items-center bg-darkgray rounded-2xl justify-center">
           <FiBell />
         </button>
         <img className="w-12 h-12 flex-shrink-0" src={"./images/Profile.png"} alt="Profile" />

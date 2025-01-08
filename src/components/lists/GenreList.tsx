11884b570d6d8e0dab358ae00a8767b4f6345034
dagijosi <dagijossy18@gmail.com>
2025-01-08 16:10:09 +0300

    refactor: update width of GenreList and MovieList components for improved layout consistency

--- src/components/lists/GenreList.tsx
+++ src/components/lists/GenreList.tsx
@@ -10,12 +10,12 @@ interface GenreCardProps {
 const GenreList: React.FC<{ genres: GenreCardProps[] }> = ({ genres }) => (
   <div className="mt-8">
     <h2 className="text-lg font-bold mb-4">Genres</h2>
-    <div className="flex flex-row justify-start w-56 overflow-x-auto mb-6 gap-4 custom-scrollbar">
+    <div className="flex flex-row justify-start w-64 overflow-x-auto mb-6 gap-4 custom-scrollbar">
       {genres.slice(0, 3).map((genre, index) => (
         <GenreCard key={index} image={genre.image} title={genre.title} />
       ))}
     </div>
-    <div className="flex flex-row justify-start w-56 overflow-x-auto gap-4 custom-scrollbar">
+    <div className="flex flex-row justify-start w-64 overflow-x-auto gap-4 custom-scrollbar">
       {genres.slice(3).map((genre, index) => (
         <GenreCard key={index} image={genre.image} title={genre.title} />
       ))}
--- src/components/lists/MovieList.tsx
+++ src/components/lists/MovieList.tsx
@@ -10,7 +10,7 @@ interface MovieCardProps {
 const MovieList: React.FC<{ title: string; movies: MovieCardProps[] }> = ({ title, movies }) => (
   <div className="mt-8">
     <h2 className="text-lg font-bold mb-4">{title}</h2>
-    <div className="flex flex-row justify-start w-56 overflow-x-auto mb-6 gap-4 custom-scrollbar">
+    <div className="flex flex-row justify-start w-64 overflow-x-auto mb-6 gap-4 custom-scrollbar">
       {movies.map((movie, index) => (
         <MovieCard key={index} image={movie.image} title={movie.title} />
       ))}
